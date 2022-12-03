2b312ff106eb58090b51c74754e608d74bb5f3c0
OwlRepo <romeoangeles015017@gmail.com>
2022-12-03 22:21:02 +0800

    feat: seo update

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -3,12 +3,12 @@ import { useRouter } from 'next/router';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'FINDROM.IO',
+  siteName: 'FINDROM.IO',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'Web portfolio of Romeo Angeles Jr whos a Front End Engineer specializing in web and mobile development by using modern framworks.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://findrom.vercel.app/',
   type: 'website',
   robots: 'follow, index',
   /**
