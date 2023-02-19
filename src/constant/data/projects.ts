3a88548bd9f218acf28ac667f5b2349199b256b9
OwlRepo <romeoangeles015017@gmail.com>
2023-02-19 17:16:09 +0800

    chore: code cleanup

---
+++ src/constant/data/projects.ts
@@ -0,0 +1,84 @@
+const projects = [
+  {
+    title: 'TALK(QUICKTALK)',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/quicktalk.png?alt=media&token=ef280e11-8ffe-4d92-96a7-513e886e14e0',
+    protected: true,
+    role: ['Android Developer', 'iOS Developer', 'Flutter'],
+    desc: "Formerly known as QuickTalk, it is an application that's focused on providing Real Time Communcations to connect brands, people and things anytime and anywhere.",
+  },
+  {
+    title: 'TALK ADMIN',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/no-banner.jpg?alt=media&token=9204eb44-8e68-48e2-9325-cd4f20d77213',
+    protected: true,
+    role: ['Web Developer', 'Product Designer', 'NextJS Developer'],
+    desc: "An application that's focused on managing the TALK Mobile Application.",
+  },
+  {
+    title: 'FIDOMONEY',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/fidomoney.png?alt=media&token=4d01353b-f2dd-45e5-969e-250865809793',
+    protected: true,
+    role: ['Flutter Developer', 'Lead Developer'],
+    desc: 'Fidomoney is one of the only companies in the financial market that exclusively supplies digital payment solutions.',
+  },
+  {
+    title: 'VIMDESK',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/vimdesk.png?alt=media&token=347c7f68-3d18-4c03-9b44-4ba2436e6865',
+    protected: true,
+    role: ['NextJS Developer', 'Lead Developer'],
+    desc: 'VimDesk is the operational console that gathers data and follows business activities extensively. With the ability to integrate and /or extract daily software tools such as ERP, MES, CRM, Finance, and more.',
+  },
+  {
+    title: 'MYNTHCHAIN',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/Screenshot_20230219_041450.png?alt=media&token=f2d1d558-71c7-4ac3-a3e2-42bfcc06aac1',
+    protected: true,
+    role: ['NextJS Developer', 'Lead Developer'],
+    desc: 'The world’s first hybrid digital marketplace for crypto collectables and non - fungible token (NFT)',
+  },
+  {
+    title: 'MYNTHDEX',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/dex.png?alt=media&token=a47c1657-8318-4dfb-b643-7132e7e65551',
+    protected: true,
+    role: ['NextJS Developer', 'Lead Developer'],
+    desc: 'A decentralized crypto exchanges that offer access to digital assets without an intermediary. Instead of relying on a company to fill and record crypto trades, MYNTHDEX offer peer-to-peer (P2P) token swaps using blockchain technology.',
+  },
+  {
+    title: 'MYNTHCHAIN ULTRAVERSE',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/ultraverse.png?alt=media&token=547c3067-ade2-497a-9c0e-9b14cc0f9757',
+    protected: true,
+    role: ['NextJS Developer', 'Lead Developer'],
+    desc: 'A decentralized infrastructure for the AR Ultraverse, merging the physical and virtual world through Augmented Reality, creating a new dimension where everything is possible.',
+  },
+  {
+    title: 'U-PRO PLUS',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/no-banner.jpg?alt=media&token=9204eb44-8e68-48e2-9325-cd4f20d77213',
+    protected: true,
+    role: ['Flutter Developer', 'Lead Developer'],
+    desc: 'A Cross Platform Mobile Application made for advertising purposes such as ADs management for Beauty Salons in Japan',
+  },
+  {
+    title: 'Digipay PH',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/digipay.png?alt=media&token=3ab4e9ee-7b0f-40fd-8e01-c1a813f28ebe',
+    protected: true,
+    role: ['NextJS Developer', 'Lead Front End Developer'],
+    desc: 'A digital payments and financial services platform with a mobile wallet, an industry leading biller ecosystem, and an interoperable backend.',
+  },
+  {
+    title: 'Back Office - Digipay PH',
+    banner:
+      'https://firebasestorage.googleapis.com/v0/b/sampleproject-31206.appspot.com/o/backoffice%20digipay.png?alt=media&token=9f3593bd-0ed8-4ff7-a183-de5782cac3c1',
+    protected: true,
+    role: ['NextJS Developer', 'Lead Front End Developer'],
+    desc: "Backoffice Digipay V2 is the redesigned and improved version of the Backoffice Digipay V1 that focuses on managing transactions and other transaction related stuff that's being made on Digipay PH",
+  },
+];
+
+export default projects;
